5a1b55f69e7421b30705c670f26f29a6fc3b4a31
GuYounes <younes.gua@gmail.com>
2018-07-05 23:21:33 +0200

    Class player, enums, config pieces

--- piece.py
+++ piece.py
@@ -1,36 +1,68 @@
 import json
-from enum import Enum
-from white import ESide
+from enums.enums import ESide, EPiece
 
+"""
+{
+    "moves": {
+		"pawn": {
+			"white": {
+				"direction": [-9, -10, -11, -20],
+				"range": 1
+			},
+			"black": {
+				"direction": [9, 10, 11, 20],
+				"range": 1
+			}
+		},
+		"rook": {
+			"direction": [ -10, 10, -1, 1 ],
+			"range": "7"
+		},
+		"bishop": {
+			"direction": [ -11, -9, 11, 9 ],
+			"range": 7
+		},
+    }
+}
+"""
 with open('config/moves.json') as movesConfigFile:
     moves = json.load(movesConfigFile).get("moves")
 
-class EPiece(Enum):
-	PAWN = 1
-	ROOK = 2
-	BISHOP = 3
-	KNIGHT = 4
-	QUEEN = 5
-	KING = 6
-
 class Piece:
 
-	def __init__(self, piece, side):
+	#piece: EPiece
+	#side: ESide
+	#coord: number
+	def __init__(self, piece, side, coord):
 		self.piece = piece
 		self.side = side
+		self.coord = coord
 		self.initRange()
 		self.initDirection()
 
 	def __str__(self): 
-		return "Pice de type {0}, camp {1}, direction {2}, range {3}".format(self.piece.name, self.side.name, self.direction, self.range)
+		return "Piece de type {0}, camp {1}, direction {2}, range {3}".format(self.piece.name, self.side.name, self.direction, self.range)
+
+	def __repr__(self):
+		return str(self) + "\n";
 
 	def initRange(self):
-		self.range = moves.get(self.piece.name.lower()).get("range");
+		if (self.piece == EPiece.PAWN):
+			self.range = moves.get(self.piece.name.lower()).get(self.side.name.lower()).get("range");
+		else: 
+			self.range = moves.get(self.piece.name.lower()).get("range");
 
 	def initDirection(self):
-		self.direction = moves.get(self.piece.name.lower()).get("direction");
+		if (self.piece == EPiece.PAWN):
+			self.direction = moves.get(self.piece.name.lower()).get(self.side.name.lower()).get("direction");
+		else:  
+			self.direction = moves.get(self.piece.name.lower()).get("direction");
 
+	# Initialize a new Piece with an object
+	# {
+	#     "coord": 1,
+	#     "type": "bishop"
+	# }
+	def init(side, object):
+		return Piece(EPiece[object.get("type").upper()], side, object.get("coord"))
 
-piece = Piece(EPiece.ROOK, ESide.WHITE)
-print(piece)
-print(moves)
--- player.py
+++ player.py
@@ -1,11 +1,25 @@
-from enum import Enum
+import json
+from piece import Piece
+from enums.enums import ESide, EPiece
 
-class ESide(Enum):
-	WHITE = 1
-	BLACK = 2
-	NEUTRAL = 3
+with open('config/pieces.json') as piecesConfigFile:
+    piecesCoords = json.load(piecesConfigFile)
 
 class Player:
 
+	# side: ESide
 	def __init__(self, side):
 		self.side = side
+		self.initPiece()
+
+	def initPiece(self):
+		self.pieces = [];
+		for piece in piecesCoords.get(self.side.name.lower()):
+			self.pieces.append(Piece.init(self.side, piece))
+
+	def __str__(self):
+		return "Player {0}, pieces: {1}".format(self.side.name, self.pieces)
+
+
+player = Player(ESide.WHITE)
+print(player.pieces)
