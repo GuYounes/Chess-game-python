8c8db34d0a4bfdbeb81c89c30817c481bebe89a8
add <laubron@hotmail.com>
2018-07-24 22:10:31 +0200

    avancement dans le en passant

--- src/pawn.py
+++ src/pawn.py
@@ -1,10 +1,14 @@
 from piece import Piece, EPiece, ESide
 from queen import Queen
+from bishop import Bishop
+from knight import Knight
+from rook import Rook
 
 class Pawn(Piece):
 
     def __init__(self, side, coord):
         super().__init__(side, coord, None, [])
+        self.enPassant = True
     
     def availableMoves(self, occupiedCases):
         availableMoves = []
@@ -19,8 +23,14 @@ class Pawn(Piece):
             if (Piece.isEmptyCase(occupiedCases, self.coordsFromVector(basicMove*2)) and self.firstMove):
                 availableMoves.append(self.coordsFromVector(basicMove*2))
         for move in captureMove:
-            if ((not self.isOutOfBounds(move)) and (Piece.isEnemyPiece(self.side, occupiedCases, self.coordsFromVector(move)))):
-                availableMoves.append(self.coordsFromVector(move))
+            if (not self.isOutOfBounds(move)):
+                if Piece.isEnemyPiece(self.side, occupiedCases, self.coordsFromVector(move)):
+               	    availableMoves.append(self.coordsFromVector(move))
+                if self.side == ESide.White and 23 < self.coord < 32 or self.side == ESide.Black and 31 < self.coord < 40:
+                	enPassantPawnCoord = move + 10 if self.side == ESide.White else move - 10
+                	if Piece.isEnemyPiece(self.side, occupiedCases, self.coordsFromVector(enPassantPawnCoord)):
+                		if occupiedCases[enPassantPawnCoord + self.coord].enPassant:
+                			availableMoves.append(self.coordsFromVector(move))
         return availableMoves
 
  	# Transform the PAWN when it reaches the end of the chessboard
